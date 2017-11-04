53c6e023c66229e6176ea9c0322b02755758f856
Sara <sara.peeters@gmail.com>
2017-11-04 16:34:40 +0100

    add language choice button

--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.utils import translation
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+def switch_lang(request, lang):
+    redirect_to = request.GET.get("redirect_to", "/")
+    if redirect_to == "" or not redirect_to.startswith('/'):
+        redirect_to = "/"
+    if translation.check_for_language(lang):
+        translation.activate(lang)
+        if hasattr(request, 'session'):
+            request.session[translation.LANGUAGE_SESSION_KEY] = lang
+        # else:
+        #     response.set_cookie(
+        #         settings.LANGUAGE_COOKIE_NAME, lang,
+        #         max_age=settings.LANGUAGE_COOKIE_AGE,
+        #         path=settings.LANGUAGE_COOKIE_PATH,
+        #         domain=settings.LANGUAGE_COOKIE_DOMAIN,
+        #     )
+    return redirect(redirect_to)
