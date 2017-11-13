9d1ae092f8d44aba7d704b88560bb649f0215a7f
sara@aspigirlcodes <aspigirlcodes@gmail.com>
2017-11-13 13:01:05 +0100

    fix for release

--- uniqid/settings.py
+++ uniqid/settings.py
@@ -17,12 +17,10 @@ from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -109,7 +107,7 @@ LANGUAGES = (
     ('de', _("German"))
 )
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
@@ -125,6 +123,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-if os.path.exists('uniqid/settings_local.py'):
+if os.path.exists(os.path.join(BASE_DIR, 'uniqid/settings_local.py')):
     from .settings_local import *
